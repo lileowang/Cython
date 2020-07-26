857cb0f86d0c489d9fe9835d1299d9f03bdeb9bb
lileowang <wangli20@hotmail.com>
2020-07-26 14:17:28 -0700

    Get python version in hex.

--- 03_call_c_func/demo.pyx
+++ 03_call_c_func/demo.pyx
@@ -1,10 +1,13 @@
 from libc.stdlib cimport atoi
+from cpython.version cimport PY_VERSION_HEX
 
 cpdef parse_charptr_to_py_int(char* s):
     """
     convert char* to int using c function atoi()
     """
-    
+
     assert s is not NULL, 'byte string value is NULL'
     return atoi(s)
 
+cpdef get_python_version():
+    return PY_VERSION_HEX
--- 03_call_c_func/main.py
+++ 03_call_c_func/main.py
@@ -1,4 +1,8 @@
 import demo
 
-i = demo.parse_charptr_to_py_int(b'10a')
-print(i)
+# i = demo.parse_charptr_to_py_int(b'10a')
+# print(i)
+
+# 3.8.5
+# python version (hex): 30805f0
+print(f'python version (hex): {demo.get_python_version():x}')
