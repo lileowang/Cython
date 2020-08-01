797f478b3403df295382910f71ef67a962006e0d
lileowang <wangli20@hotmail.com>
2020-08-01 01:33:31 -0700

    testing queue functions

--- 06_call_c_queue_static/Cpp_console/Cpp_console/Cpp_console.cpp
+++ 06_call_c_queue_static/Cpp_console/Cpp_console/Cpp_console.cpp
@@ -1,12 +1,27 @@
 // Cpp_console.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "pch.h"
+//#include "pch.h"
 #include <iostream>
+#include "queue.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n"; 
+    Queue * q = queue_new();
+
+    queue_push_tail(q, (QueueValue)10);
+    queue_push_tail(q, (QueueValue)20);
+
+    cout << "peek head: " << (int)queue_peek_head(q) << endl;
+    cout << "pop head: " << (int)queue_pop_head(q) << endl;
+    cout << "pop head: " << (int)queue_pop_head(q) << endl;
+    cout << "pop head: " << (int)queue_pop_head(q) << endl;
+
+    if (q != NULL)
+    {
+        queue_free(q);
+    }
+
 }
