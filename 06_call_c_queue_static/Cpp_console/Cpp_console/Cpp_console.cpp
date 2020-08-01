52e6bce8a04c8d76247e777557d6a70bc033d643
lileowang <wangli20@hotmail.com>
2020-08-01 01:13:22 -0700

    created console program to test c functions.

---
+++ 06_call_c_queue_static/Cpp_console/Cpp_console/Cpp_console.cpp
@@ -0,0 +1,12 @@
+// Cpp_console.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n"; 
+}
